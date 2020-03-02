53596a1e178e02df4385cdc0d9e5c49497ff897a
licaiying <1194449590@qq.com>
2020-03-02 19:33:05 +0800

    全局样式配置

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ import 'vant/lib/index.css'
 // 导入配置font-size的基准值文件
 import 'amfe-flexible/index.min.js'
 
+// 导入全局样式控制文件
+// 注意：在vant的css样式导入之后设置,目的是：为了覆盖vant的样式
+import '@/assets/css/global.less'
+
 Vue.use(Vant)
 
 Vue.config.productionTip = false
