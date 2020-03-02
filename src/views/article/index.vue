266de0580db18229a4791fbdf25b7016cb130744
licaiying <1194449590@qq.com>
2020-03-02 17:35:18 +0800

    初始化项目

---
+++ src/views/article/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: ''
+}
+</script>
+
+<style scoped lang="less" >
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    port: '12233' // 新的端口号，默认的是8080
+  }
 }
