e85c13b8a574ab03b20695cc496b353ff8eb5906
licaiying <1194449590@qq.com>
2020-03-05 18:53:56 +0800

    登录组件-中文语言提示设置

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,8 +1,18 @@
 // 按需导入extend函数
-import { extend } from 'vee-validate'
+// 1.导入localize函数，以实现语言配置
+import { extend, localize } from 'vee-validate'
 
 // 导入全部规则
 import * as rules from 'vee-validate/dist/rules'
+
+// 2. 导入需要的语言包
+import zhCN from 'vee-validate/dist/locale/zh_CN.json'
+
+// 3. 注册语言包(对象成员简易赋值 zhCN:zhCN)
+localize({ zhCN })
+
+// 4. 激活当前语言
+localize('zhCN')
 
 // rules:是一个大对象，里面包括导入的各个规则成员
 // Object.keys(rules): 返回该对象全部成员名称，以"数组"形式返回 ['required','length','email'……]
