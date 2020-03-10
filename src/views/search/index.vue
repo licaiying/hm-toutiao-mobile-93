867b6b2490656689155405d36323f5e26a80326e
licaiying <1194449590@qq.com>
2020-03-10 23:54:48 +0800

    搜索组件-获取联想关键字时的防抖处理

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -25,7 +25,11 @@ export default {
 
   // 设置watch监听器，以监听 搜索框中 用户输入的内容子段，以此来实现联想关键字的匹配
   watch: {
-    searchText: async function (newV) {
+    searchText: function (newV) {
+      // 该监听器每次进来的时候，首先清除 this.timer（就是setTimeout停止）定时器
+      // 通过定时器，清除的是输入两个字符时，之间相隔的时间差
+      clearTimeout(this.timer)
+
       // 用户没有输入信息不要联想
       if (!newV) {
         // 去除旧的联想数据
@@ -33,10 +37,19 @@ export default {
         return false
       }
 
-      // 正常联想
-      const result = await apiSearchSuggestion(newV)
-      // console.log(result) // {options: Array(5)}
-      this.suggestionList = result.options
+      // 通过定时器实现 防抖
+      // 防抖技术要介入
+      // 对setTimeout的返回结果进行接收，以便可以中断其执行
+      // 会返回"整型数字"信息
+      // 给当前组件临时声明一个timer的临时成员，这个成员不用事先通过data声明
+      // 组件运行期间都生效，使得就是唯一的timer被使用(即使事件被多次调用)
+      // this.timer表示给 组件实例 动态增加一个data成员，临时使用的可以不用提前声明
+      this.timer = setTimeout(async () => {
+        // 正常联想
+        const result = await apiSearchSuggestion(newV)
+        // console.log(result) // {options: Array(5)}
+        this.suggestionList = result.options
+      }, 1000)
     }
   }
 }
